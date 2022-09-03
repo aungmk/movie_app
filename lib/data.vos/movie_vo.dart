6d77a20a22a7bd1f2a2b0be65f92b77353456a25
aungmk <aunglay265@gmail.com>
2022-09-03 22:22:37 +0630

    retrofit generated with null safety operator

--- lib/data.vos/movie_vo.dart
+++ lib/data.vos/movie_vo.dart
@@ -5,46 +5,46 @@ part 'movie_vo.g.dart';
 @JsonSerializable()
 class MovieVO {
   @JsonKey(name:"adult")
-  late bool adult;
+   bool? adult;
 
   @JsonKey(name:"backdrop_path")
-  late String backDropPath;
+   String? backDropPath;
 
   @JsonKey(name:"genre_ids")
-  late List<int> genreIds;
+   List<int?> genreIds;
 
   @JsonKey(name:"id")
-  late int id;
+   int? id;
 
   @JsonKey(name:"original_language")
-  late String originalLanguage;
+   String? originalLanguage;
 
   @JsonKey(name:"original_title")
-  late String originalTitle;
+   String? originalTitle;
 
   @JsonKey(name:"overview")
-  late String overview;
+   String? overview;
 
   @JsonKey(name:"popularity")
-  late double popularity;
+   double? popularity;
 
   @JsonKey(name:"poster_path")
-  late String posterPath;
+   String? posterPath;
 
   @JsonKey(name:"release_date")
-  late String releaseDate;
+   String? releaseDate;
 
   @JsonKey(name:"title")
-  late String title;
+   String? title;
 
   @JsonKey(name:"video")
-  late bool video;
+   bool? video;
 
   @JsonKey(name:"vote_average")
-  late double voteAverage;
+   double? voteAverage;
 
   @JsonKey(name:"vote_count")
-  late int voteCount;
+   int? voteCount;
 
   MovieVO(
       this.adult,
--- lib/network/retrofit_data_agent_impl.dart
+++ lib/network/retrofit_data_agent_impl.dart
@@ -5,7 +5,7 @@ import 'package:movie_app/network/movie_data_agent.dart';
 import 'package:movie_app/network/the_movie_api.dart';
 
 class RetrofitDataAgentImpl extends MovieDataAgent {
-   late TheMovieApi mApi;
+    TheMovieApi? mApi;
 
   RetrofitDataAgentImpl(){
     final dio= Dio();
@@ -14,7 +14,7 @@ class RetrofitDataAgentImpl extends MovieDataAgent {
 
   @override
   void getNowPlayingMovies(int page) {
-    mApi.getNowPlayingMovie(API_KEY, LANGUAGE_EN_US, page.toString())
+    mApi?.getNowPlayingMovie(API_KEY, LANGUAGE_EN_US, page.toString())
         .then((value){
       debugPrint("NOW PLAYING Movies......${value.toString()}");
     }).catchError((error){
