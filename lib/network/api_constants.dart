b6f3220d484e33a596fa70828fcd3c76b5fbc322
aungmk <aunglay265@gmail.com>
2022-09-13 19:59:43 +0630

    finished but genres section not ok

--- lib/network/api_constants.dart
+++ lib/network/api_constants.dart
@@ -6,7 +6,7 @@ const String IMAGE_BASE_URL="http://image.tmdb.org/t/p/w400/";
 const String ENDPOINT_GET_NOW_PLAYING="/3/movie/now_playing";
 const String ENDPOINT_GET_POPULAR="/3/movie/popular";
 const String ENDPOINT_GET_TOP_RATED="/3/movie/top_rated";
-const String ENDPOINT_GET_GENRES="/3/movie/list";
+const String ENDPOINT_GET_GENRES="/3/genre/movie/list";
 const String ENDPOINT_GET_MOVIES_BY_GENRE="/3/list";
 const String ENDPOINT_GET_ACTORS="/3/person/popular";
 
